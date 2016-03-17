06ba3945ca51c9ea620ea236d4dcf93361facf39
BambooIV <ningdisheng@gmail.com>
2016-03-17 22:29:56 +0800

    update

---
+++ ubuntu/d.sh
@@ -0,0 +1,5 @@
+echo -e "\033[31m======clean======\033[0m"  
+rm -rf /home/bamboo/.local/share/Trash/files/*
+echo -e "\033[31m======bingo======\033[0m"
+echo -e "\033[31m======exiting======\033[0m"
+exit
